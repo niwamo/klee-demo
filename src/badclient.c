4f61a77497caf80b6025ecba19d2590a6b28caf4
niwamo <nm@niwamo.com>
2026-01-07 22:40:25 -0600

    badclient

--- src/badclient.c
+++ src/badclient.c
@@ -8,22 +8,27 @@
 #define PORT 8080
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("Usage: %s <message>\n", argv[0]);
+  if (argc < 3) {
+    printf("Usage: %s <size to report> <message>\n", argv[0]);
     return 1;
   }
 
   int sock = 0;
   struct sockaddr_in serv_addr;
-  char *message = argv[1];
-  size_t msg_len = strlen(message);
+  char *message = argv[2];
 
-  unsigned char packet[msg_len];
+  size_t msg_len = atoi(argv[1]);
+  size_t actual_len = strlen(argv[2]);
+
+  unsigned char packet[actual_len + 5];
   packet[0] = 0x01; // Plaintext message type
 
   uint32_t net_len = htonl(msg_len);
   memcpy(packet + 1, &net_len, sizeof(uint32_t));
-  memcpy(packet + 5, message, msg_len);
+
+  const char *msg_ptr = (char *)(packet + 5);
+  memcpy(packet + 5, message, actual_len);
+  printf("Measured length: %lu", strlen(msg_ptr));
 
   // Standard Socket Setup
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -44,8 +49,9 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  send(sock, packet, 5 + msg_len, 0);
-  printf("Message sent (Type=0x01, Len=%zu)\n", msg_len);
+  send(sock, packet, 5 + actual_len, 0);
+  printf("Message sent (Type=0x01, Len=%zu, Actual Len=%zu)\n", msg_len,
+         actual_len);
 
   close(sock);
   return 0;
--- src/server.c
+++ src/server.c
@@ -7,8 +7,9 @@ void process_packet(unsigned char *data, ssize_t size) {
   if (size < 5)
     return;
 
-  unsigned char type = data[0];
+  char type = data[0];
 
+  // get size of message and allocate buffer
   uint32_t msg_len;
   memcpy(&msg_len, data + 1, sizeof(uint32_t));
   msg_len = ntohl(msg_len);
@@ -16,9 +17,10 @@ void process_packet(unsigned char *data, ssize_t size) {
 
   printf("Processing Message Type %02x of len %d\n", type, msg_len);
 
-  memcpy(msg, data + 5, msg_len);
-
+  // copy message and ensure string termination
+  memcpy(msg, data + 5, size - 5);
   msg[msg_len] = '\0';
+
   printf("Message: %s\n", msg);
 }
 
@@ -27,7 +29,7 @@ int main() {
   struct sockaddr_in address;
   int opt = 1;
   int addrlen = sizeof(address);
-  unsigned char buffer[1024] = {0};
+  unsigned char buffer[8192] = {0};
 
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
@@ -42,7 +44,7 @@ int main() {
   printf("Server listening on port 8080...\n");
   while ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                               (socklen_t *)&addrlen))) {
-    ssize_t valread = read(new_socket, buffer, 1024);
+    ssize_t valread = read(new_socket, buffer, 8192);
     process_packet(buffer, valread);
     close(new_socket);
   }
